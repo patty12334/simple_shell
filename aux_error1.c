04da9a41b38c7ccefc1ea44bd2a65f1efa073d8c
MaryBaptista <marybaptistanwachukwu@gmail.com>
2022-06-16 18:31:54 -0700

    aux_error1.c

--- aux_error1.c
+++ aux_error1.c
@@ -1,14 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* strcat_cd - function that concatenates the message for cd error
-*
-* @datash: data relevant (directory)
-* @msg: message to print
-* @error: output message
-* @ver_str: counter lines
-* Return: error message
-*/
+ * strcat_cd - function that concatenates the message for cd error
+ *
+ * @datash: data relevant (directory)
+ * @msg: message to print
+ * @error: output message
+ * @ver_str: counter lines
+ * Return: error message
+ */
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 {
 	char *illegal_flag;
@@ -37,12 +37,11 @@ char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 	_strcat(error, "\0");
 	return (error);
 }
-
 /**
-* error_get_cd - error message for cd command in get_cd
-* @datash: data relevant (directory)
-* Return: Error message
-*/
+ * error_get_cd - error message for cd command in get_cd
+ * @datash: data relevant (directory)
+ * Return: Error message
+ */
 char *error_get_cd(data_shell *datash)
 {
 	int length, len_id;
@@ -78,10 +77,10 @@ char *error_get_cd(data_shell *datash)
 }
 
 /**
-* error_not_found - generic error message for command not found
-* @datash: data relevant (counter, arguments)
-* Return: Error message
-*/
+ * error_not_found - generic error message for command not found
+ * @datash: data relevant (counter, arguments)
+ * Return: Error message
+ */
 char *error_not_found(data_shell *datash)
 {
 	int length;
@@ -108,13 +107,12 @@ char *error_not_found(data_shell *datash)
 	free(ver_str);
 	return (error);
 }
-
 /**
-* error_exit_shell - generic error message for exit in get_exit
-* @datash: data relevant (counter, arguments)
-*
-* Return: Error message
-*/
+ * error_exit_shell - generic error message for exit in get_exit
+ * @datash: data relevant (counter, arguments)
+ *
+ * Return: Error message
+ */
 char *error_exit_shell(data_shell *datash)
 {
 	int length;
