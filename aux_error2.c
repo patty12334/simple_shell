544bfd2fe75f4e7fe61f75fab50062ca5677ee0b
MaryBaptista <marybaptistanwachukwu@gmail.com>
2022-06-16 18:41:11 -0700

    aux_error2.c

--- aux_error2.c
+++ aux_error2.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* error_env - error message for env in get_env.
-* @datash: data relevant (counter, arguments)
-* Return: error message.
-*/
+ * error_env - error message for env in get_env.
+ * @datash: data relevant (counter, arguments)
+ * Return: error message.
+ */
 char *error_env(data_shell *datash)
 {
 	int length;
@@ -36,11 +36,11 @@ char *error_env(data_shell *datash)
 	return (error);
 }
 /**
-* error_path_126 - error message for path and failure denied permission.
-* @datash: data relevant (counter, arguments).
-*
-* Return: The error string.
-*/
+ * error_path_126 - error message for path and failure denied permission.
+ * @datash: data relevant (counter, arguments).
+ *
+ * Return: The error string.
+ */
 char *error_path_126(data_shell *datash)
 {
 	int length;
